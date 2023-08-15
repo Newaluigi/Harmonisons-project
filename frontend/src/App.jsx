d9d8c754df4e2d002877ce776a2fae6da46238cd
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-08-15 16:38:27 +0200

    set up project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+
+// import "./App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <p>coucou</p>
+    </div>
+  )
+}
+
+export default App
