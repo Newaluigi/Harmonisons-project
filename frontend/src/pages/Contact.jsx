d9d95354cc707ce0229b7e415567b679a84bc4e9
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-08-21 01:15:22 +0200

    contact form added and stylized

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,8 +19,44 @@ export default function Contact() {
       <section id="ContactForm">
         <h2 className="sectionTitle">Une question ?</h2>
         <p className="genericParagraph"></p>
+        <form>
+          <div className="input-group">
+            <input
+              name="name"
+              type="text"
+              className="input-group__input"
+              required
+            />
+            <label htmlFor="name" className="input-group__label">
+              Nom
+            </label>
+          </div>
+          <div className="input-group">
+            <input
+              name="email"
+              type="text"
+              className="input-group__input"
+              required
+            />
+            <label htmlFor="email" className="input-group__label">
+              Email
+            </label>
+          </div>
+          <div className="input-group">
+            <textarea
+              name="text"
+              className="input-group__input"
+              required
+            ></textarea>
+            <label htmlFor="message" className="input-group__label">
+              Votre message
+            </label>
+          </div>
+          <button type="submit" className="genericButton">
+            Envoyer
+          </button>
+        </form>
       </section>
-      <p>Je suis le contact</p>
     </div>
   )
 }
