d1a91217b5ff4165faff04b79923a918d331888c
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-08-22 04:45:45 +0200

    fix bug router

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,7 +4,8 @@ const router = express.Router()
 
 const articlesControllers = require("./controllers/articlesControllers")
 
-router.get("/articles", articlesControllers.browse)
+router.get("/articles", articlesControllers.browsePublishedArticles)
+router.get("/archivedarticles", articlesControllers.browseArchivedArticles)
 router.get("/articles/:id", articlesControllers.read)
 router.post("/articles", articlesControllers.add)
 router.put("/articles/:id", articlesControllers.edit)
